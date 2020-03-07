1a854ac1ce3268f643c69d544db31e80d14b1834
NicholaswithAK <ncl1984@gmail.com>
2020-03-06 23:07:38 -0700

    initial commit

--- src/components/info.js
+++ src/components/info.js
@@ -14,33 +14,30 @@ export default function info() {
       <div className="my-5"></div>
       <div className="display-3" style={{ color: "#FF8A00" }}>
         <span style = {{color : "white"}}>Hello, I'm</span> <br />
-        Sumanth.
+        Nicholas.
       </div>
-      <div className="h1 code mt-4 mb-3">async {"{"}</div>
+      <div className="h1 code mt-4 mb-3">whoIam {"{"}</div>
       <div className="text-muted mx-5 my-4 h3 text-justify info">
-        A Passionate Front-end Developer, UI/UX Enthusiast, Android Geek and Web
-        Developer.
-        <br /> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-        eiusmod tempor incididunt ut labore et.
+        A Passionate Full Stack Web Developer, UI/UX Developer.
       </div>
       <div className="h1 code mt-2 mb-3">{"}"}</div>
       <div className="h1 mt-5">
-        <a className="mr-5 icon" href="https://twitter.com/Mr404Found">
+        <a className="mr-5 icon" href="https://twitter.com/pizza_papiii_99">
           <Twitter />
         </a>
-        <a className="mr-5 icon" href="https://instagram.com/sumanth.zero7">
+        <a className="mr-5 icon" href="https://instagram.com/pizza_papiii_99">
           <Insta />
         </a>
-        <a className="mr-5 icon" href="https://github.com/Mr404Found">
+        <a className="mr-5 icon" href="https://github.com/Nicholaswithak">
           <Github />
         </a>
         <a
           className="mr-5 icon"
-          href="https://www.youtube.com/channel/UCw0FDIL-HpcfzEZrjcQwhMQ"
+          href="https://www.youtube.com"
         >
           <Youtube />
         </a>
-        <a className="mr-5 icon" href="mailto://contactmr404here@gmail.com">
+        <a className="mr-5 icon" href="mailto://nick@latenightjunkfood.com">
           <Mail />
         </a>
       </div>
